ae15325a4f4ec7d1fd055669095302de35e67aff
Conor C <Conorc1000@gmail.com>
2015-11-07 17:28:24 +0000

    stripe

--- public/js/membership.js
+++ public/js/membership.js
@@ -1,1 +1,10 @@
+var paymentForm=document.getElementById('payment-form');
+paymentForm.addEventListener('submit', function(event){
+  console.log("submit");
+event.preventDefault();
 
+  Stripe.card.createToken(paymentForm, stripeResponseHandler(status, response){
+    var token = response.id
+    console.log(token);
+  });
+})
