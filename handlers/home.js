876e0d0f8775080b35256cfc57e42d0724368a54
sohilpandya <sohilpandya@me.com>
2015-11-08 12:04:06 +0000

    made live counter using pusher in outfit.html

--- handlers/home.js
+++ handlers/home.js
@@ -9,27 +9,6 @@ var counter = 0;
 module.exports = function home(req, res){
   res.writeHead(200, {'Content-Type':'text/html'});
 
-  var pusher = new Pusher({
-    appId: '152826',
-    key: 'f5049b557c6eb19e1ae9',
-    secret: '75da3e2cd0aa7b7004e2',
-    encrypted: true
-  });
-  pusher.port = 443;
 
-  counter = counter + 1;
-
-setTimeout(function(){
-  pusher.trigger('notifications', 'new_notification', {
-    message: "A new Member has Joined!!!",
-    counter: counter
-  });
-},400)
-
-  pusher.trigger('disconnection','new_notification',{
-    message: "Client has disconnected"
-  });
-
-
-  res.end(index,"Notification triggered!");
+  res.end(index);
 };
